56a7432c81cef0e910c727b1a3cba859128d970b
sararob <sararob@brandeis.edu>
2013-04-14 11:08:12 -0700

    Update vote count in API model

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -24,7 +24,11 @@ class VotesController < ApplicationController
 
           if @vote.save
             
-            #Update vote count in Api model...figure out how to do this
+            #Update vote count in API model
+            @api = Api.find(params[:api_id])
+            @numvotes = Vote.where("api_id = ?", params[:api_id]).count
+            @api["votes"] = @numvotes
+            @api.save
             
             flash[:notice] = "Your vote was added"
             redirect_to root_path
