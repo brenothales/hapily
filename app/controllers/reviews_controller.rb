e7db18ec8c602f2a17d12e182ff167427946470c
sararob <sararob@brandeis.edu>
2013-04-16 15:54:48 -0700

    Add redirect to new review link

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,9 @@
 class ReviewsController < ApplicationController
   def new
+    unless signed_in?
+      flash[:error] = "You must be signed in to write a review."
+      redirect_to new_user_session_path
+    end
     @review = Review.new(params[:review])
   end
   
