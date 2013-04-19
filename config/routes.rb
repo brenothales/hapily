393904c31b1240e0b5423288843897be3c73e6a8
sararob <sararob@brandeis.edu>
2013-04-18 22:43:48 -0700

    Fix routes on API show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,27 @@
 Hapily::Application.routes.draw do
   devise_for :users
-  get 'profile(/:user_id)' => 'users#show', :as => :profile
-  get 'all_users' => 'users#index'
 
   root :to => "apis#index"
   
-  resources :apis do 
-    #get 'apis(/:id)' => 'apis#show', :as => :apipage
-    collection do
-      get '/search' => 'apis#search'
-      get '(/:tag)' => 'apis#index'
-    end
-  end
-  
+  resources :apis
   resources :votes
   resources :charges
   resources :reviews
+  
+  get "static_pages/home"
 
   get "about" => "static_pages#about"
+  
+  get 'apis(/:tag)' => 'apis#index'
   
   get 'categories/new' => 'categories#new', :as => :new_category
   post 'categories/create' => 'categories#create'
   
+  get 'profile(/:user_id)' => 'users#show', :as => :profile
+  get 'all_users' => 'users#index'
   
-  
-  
-  
+  get 'apis/search' => 'apis#search'
+  get 'apis(/:id)' => 'apis#show', :as => :apipage
   
    
 
