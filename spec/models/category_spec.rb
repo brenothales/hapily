38fc7c2e29ca249d28a202a22553aafdcdf40add
sararob <sararob@brandeis.edu>
2013-04-25 16:13:12 -0700

    Write category tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,18 @@ require 'spec_helper'
 describe Category do
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of(:name).case_insensitive }
+  
+  context "validates name uniqueness" do
+    it "should not save" do
+      first_category = Category.new name: 'Test Category'
+      first_category.save
+      
+      invalid_category = Category.new name: 'Test Category'
+      invalid_category.save.should eq false
+    end
+    it "should save" do
+      valid_category = Category.new name: 'Unique Category'
+      valid_category.save.should eq true
+    end  
+  end
 end
