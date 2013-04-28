3ff37b203a4f5758abde3071c0976dcb4905eea3
sararob <sararob@brandeis.edu>
2013-04-28 16:55:06 -0700

    Write tests for gravatar in users helper

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
 describe UsersHelper do
+  user_with_gravatar = User.new name: 'Test User', email: 'sararob@brandeis.edu', password: 'foobarbaz'
+  gravatar_id = Digest::MD5::hexdigest(user_with_gravatar.email.downcase)
+  size = 75
+  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   it "should pull the User's image from Gravatar" do
-    user_with_gravatar = User.new name: 'Test User', email: 'sararob@brandeis.edu', password: 'foobarbaz'
     gravatar_for(user_with_gravatar).should_not be_nil
+  end
+
+  it "should find the user's gravatar id" do
+    gravatar_id.should_not be_nil
+  end
+
+  it "should create the correct url given the id and size" do
+
+    gravatar_url.should eq "https://secure.gravatar.com/avatar/#{gravatar_id}?s=75"
+  end
+
+  it "should create an image tag from the url" do
+    image_tag(gravatar_url).should_not be_nil
   end
 end
