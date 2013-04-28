17a448673ec875322e35b42f6580e992b70fdcee
sararob <sararob@brandeis.edu>
2013-04-28 16:53:21 -0700

    Write tests for already_voted_on? method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,18 @@ describe User do
 end
 
 describe "#already_voted_on?" do
+  it "should get the votes for each user" do
+    vote = FactoryGirl.create(:vote)
+    user = FactoryGirl.create(:user, id: 1)
+    api = FactoryGirl.create(:api, id: 1)
+    user.votes.where(api_id: 1).count.should eq 1
+  end
   it "should be true if user has already voted on the API" do
-    user_with_vote = FactoryGirl.create(:user, id: 1)
-    already_voted = FactoryGirl.create(:vote)
-    test_api = FactoryGirl.create(:api, id: 1)
+    api_with_votes = FactoryGirl.create(:api, id: 1)
+    api_with_votes.votes.create api_id: 1, user_id: 1
+    user_with_votes = FactoryGirl.create(:user, id: 1)
     
-    user_with_vote.already_voted_on?(test_api).should eq true
+    user_with_votes.already_voted_on?(api_with_votes).should eq true
   end
   
   it "should be false if user has not voted on the API" do
