5bdd66d92fee943a4afbabeec330e8327d6a1157
sararob <sararob@brandeis.edu>
2013-04-28 16:54:22 -0700

    Write tests for reviews controller

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe ReviewsController do
-
+  before(:each) do
+    @api = FactoryGirl.create(:api, id: 1)
+  end
+  after(:each) do
+    Api.delete_all
+    Review.delete_all
+  end
   describe "GET #new" do
     context "user signed in" do
       it "should show 'Add Review' button" do
-        get :show, api_id: 1
-        it { should.have_selector('a', text: 'Add a review')}
+        visit apipage_path(id: 1)
+        page.should have_content('Add a review')
       end
       it "should assign a new review to @review" do
         get :new
@@ -15,20 +21,45 @@ describe ReviewsController do
     end
 
     context "user not signed in" do
-      it "should not show 'Add Review' button"
-      it "should show sign in link"
-      it "should redirect to sign in page if user clicks sign in link"
+      it "should show sign in link" do
+        visit apipage_path(id: 1)
+        page.should have_content('Sign in to add a review')
+      end
+      it "should redirect to sign in page if user clicks sign in link" do
+        click_link("Sign in")
+        response.should redirect_to new_user_session_path
+      end
     end
   end
 
   describe "POST #create" do
     context "with valid attributes" do
-      it "should save the review to the database"
-      it "should respond to Ajax"
+      it "should save the review to the database" do
+         expect {
+           @api.reviews.create rating: 5, thoughts: "lorem ipsum"
+         }.to change(Review,:count).by(1)
+      end
+      it "should show the flash[:notice]" do
+        post :create, review: FactoryGirl.attributes_for(:review)
+        assigns(:review)
+        flash[:notice].should_not be_nil
+      end
+
     end
 
     context "with invalid attributes" do
-      it "should not save the review"
+      it "should not save the review" do
+        expect {
+          @api.reviews.create rating: 4, thoughts: nil
+        }.to_not change(Review, :count)
+      end
+      it "should show the flash[:error]" do
+        post :create, review: FactoryGirl.attributes_for(:review, rating: nil, thoughts: "")
+        assigns(:review)
+        flash[:error].should_not be_nil
+      end
     end
+
+    it "should respond to Ajax"
   end
 end
