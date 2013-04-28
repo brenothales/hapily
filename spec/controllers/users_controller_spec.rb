8cb8bf67b91f5239977a0c50fe3e16aa2b371220
sararob <sararob@brandeis.edu>
2013-04-28 16:55:43 -0700

    Write tests for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,23 +1,18 @@
 require 'spec_helper'
 
 describe UsersController do
+  login_user
   
   describe "User pages" do
-    user = FactoryGirl.create(:user)
 
   describe "User show page" do
-    it "fetches the right current user" do
-      User.find(user.id).should eq user
+    it "assigns the right user to @current_user" do
+      User.find(subject.current_user.id).should eq subject.current_user
     end
 
-    it "show the right created at date" do
+    it "assigns the right date to @member_since" do
       current_date = Time.now.strftime("%B %d, %Y")
-      user.created_at.strftime("%B %d, %Y").should eq current_date
-    end
-
-    it "renders the show view" do
-      get :show, id: user.id
-      response.should render_template :show
+      subject.current_user.created_at.strftime("%B %d, %Y").should eq current_date
     end
   end
 
@@ -25,6 +20,12 @@ describe UsersController do
     it "should render the index view" do
       get :index
       response.should render_template :index
+    end
+
+    it "should paginate 10 users per page" do
+      30.times { FactoryGirl.create(:user) }
+      visit all_users_path
+      page.should have_selector('div.pagination')
     end
   end
   end
