f29128db253248d0b768a16dafb8006797e16eac
sararob <sararob@brandeis.edu>
2013-04-27 17:04:11 -0700

    Write factory tests for user show and index page

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,23 +4,30 @@ describe "Users" do
  
   subject { page }
   
-  describe "users index page" do
+  describe "user pages" do
     
-    let(:user) { FactoryGirl.create(:user) }
-    
-    before(:all) { 30.times { FactoryGirl.create(:user) } }
-    after(:all) { User.delete_all }
+    user = FactoryGirl.create(:user)
+    second_user = FactoryGirl.create(:user, name: "Sara", id: 3, email: "sararob@brandeis.edu")
     
     before(:each) do
-      new_user_session user
       visit all_users_path
     end
     
     describe "pagination" do
-      User.paginate(page: 1).each do |user|
-        page.should have_link(user.name, href: profile_path(user_id: user.id))
-        page.should have_content(user.name)
+      
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_link(user.name, href: profile_path(user_id: user.id))
+          
+          page.should have_link(second_user.name, href: profile_path(user_id: second_user.id))
+        end
       end 
+    end
+    
+    describe "user show page" do
+      
     end
   end
 end
+
+#Add tests to make sure signed in users see the right header links
