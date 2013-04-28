d846566eeeee421cad32c87265290c622ee08b79
sararob <sararob@brandeis.edu>
2013-04-27 17:01:21 -0700

    Write tests for api mailer

---
+++ spec/mailers/api_mailer_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe ApiMailer do
+  describe '#submit_api' do
+    api_to_email = FactoryGirl.create(:api)
+    @user = FactoryGirl.create(:user, id: 1)
+    let(:mail) { ApiMailer.submit_api(api_to_email)}
+
+    it "should send the email to me" do
+      mail.to.should eq ["sararob@brandeis.edu"]
+    end
+
+    it "should have the right subject" do
+      mail.subject.should eq api_to_email.name + ' added to hapily'
+    end
+
+    it "has the API name in the email body" do
+      mail.body.raw_source.should include api_to_email.name
+    end
+
+    it "has the external API url in the email body" do
+      mail.body.raw_source.should include api_to_email.url
+    end
+
+    it "has the API show page url in the email body" do
+      mail.body.raw_source.should include "http://hapily.herokuapp.com/apis/show/" + api_to_email.id.to_s
+    end
+
+    it "has the API category in the email body" do
+      mail.body.raw_source.should include api_to_email.category
+    end
+  end
+end
